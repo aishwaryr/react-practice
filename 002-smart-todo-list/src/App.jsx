e799fc1cf844dc52d107e4465f23140a982349a9
aishwaryr <araj713@gmail.com>
2026-02-10 13:31:36 +0530

    002 - add done/pending status, toggle status and delete todo

--- 002-smart-todo-list/src/App.jsx
+++ 002-smart-todo-list/src/App.jsx
@@ -1,13 +1,31 @@
 import { useState } from "react";
 
+let todoID = 1;
+
 function App() {
+  const TODO = "todo";
+  const DONE = "done";
+
   const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
-    setTodos((prevTodos) => [...prevTodos, todo]);
+    setTodos((prevTodos) => [...prevTodos, { id: todoID, todo, status: TODO }]);
+    todoID++;
     setTodo("");
+  }
+
+  function toggleStatus(id) {
+    setTodos((prevTodos) =>
+      prevTodos.map((t) =>
+        t.id === id ? { ...t, status: t.status === DONE ? TODO : DONE } : t,
+      ),
+    );
+  }
+
+  function deleteTodo(id) {
+    setTodos((prevTodos) => prevTodos.filter((t) => t.id !== id));
   }
 
   return (
@@ -25,7 +43,17 @@ function App() {
         {todos.length > 0 ? (
           <ul>
             {todos.map((t) => (
-              <li>{t}</li>
+              <li key={t.id} className={t.status === DONE ? "completed" : ""}>
+                <span>
+                  <input
+                    type="checkbox"
+                    checked={t.status === DONE}
+                    onChange={() => toggleStatus(t.id)}
+                  />
+                </span>
+                <span>{t.todo}</span>
+                <span onClick={() => deleteTodo(t.id)}>x</span>
+              </li>
             ))}
           </ul>
         ) : (
